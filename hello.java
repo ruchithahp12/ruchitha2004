68a222ef3af21a91b80a30381b6029b176299c56
spt <129258745+ruchithahp12@users.noreply.github.com>
2023-04-26 11:13:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 class hello 
 {
-	public static void main (String[]arge)
+	public static void main (String[] args)
 	{
-		System.out.println("hello world")
-		System.out.println("spt cse 4 sem")
-		System.out.println("ruchitha")
+		System.out.println("hello world");
+		System.out.println("spt cse 4 sem");
+		System.out.println("ruchitha");
 	}
 }
